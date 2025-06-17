6ab67bf27c4bab30f0176416d1c9d708d023ec0d
adams <adams@lapins.lv>
2025-06-17 11:15:19 +0300

    did some formatting...

--- circuit-python/code.py
+++ circuit-python/code.py
@@ -1,17 +1,17 @@
 # import necessary libs
-import usb_hid
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_circuitplayground.express import cpx
+import usb_hid  # type: ignore
+from adafruit_hid.keyboard import Keyboard  # type: ignore
+from adafruit_hid.keycode import Keycode  # type: ignore # noqa: F401
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS  # type: ignore
+from adafruit_circuitplayground.express import cpx  # type: ignore
 from time import sleep
 import random
-import analogio
-import board
+import analogio  # type: ignore
+import board  # type: ignore
 from gc import collect
 
-def joystick():
 
+def joystick():
     vrx = analogio.AnalogIn(board.A1)
     vry = analogio.AnalogIn(board.A2)
     sw = analogio.AnalogIn(board.A3)
@@ -24,7 +24,6 @@ def joystick():
 
     while True:
         try:
-
             x = vrx.value
             y = vry.value
             s = sw.value
@@ -61,55 +60,54 @@ def joystick():
             collect()
 
         except MemoryError:
-
             print(f"Memory error: only {gc.mem_free()} bytes free\n")
 
-def colwheel(n, brightness):
 
+def colwheel(n, brightness):
     if n == 0:
         return (0, 0, 0)
     if n > 0 and n <= 85:
-        return tuple([i*brightness for i in (0, int(255-n*3), int(3*n))])
+        return tuple([i * brightness for i in (0, int(255 - n * 3), int(3 * n))])
     if n > 85 and n <= 170:
         n -= 85
-        return tuple([i*brightness for i in (int(n*3), 0, int(255-n*3))])
+        return tuple([i * brightness for i in (int(n * 3), 0, int(255 - n * 3))])
     if n > 170 and n <= 255:
         n -= 170
-        return tuple([i*brightness for i in (int(255-n*3), int(n*3), 0)])
+        return tuple([i * brightness for i in (int(255 - n * 3), int(n * 3), 0)])
+
 
 def tone():
-
-    if cpx.button_a: # press button_a for tone
-        cpx.start_tone(10) #freq for tone
+    if cpx.button_a:  # press button_a for tone
+        cpx.start_tone(10)  # freq for tone
     elif cpx.button_b:
-        cpx.start_tone(900) #freq for tone
+        cpx.start_tone(900)  # freq for tone
     else:
-        cpx.stop_tone() # tone plays while button is pressed
+        cpx.stop_tone()  # tone plays while button is pressed
+
 
 def passwords():
-
     kbd = Keyboard(usb_hid.devices)
     layout = KeyboardLayoutUS(kbd)
 
     while True:  # Keep listening for button presses
-
         if cpx.button_a:
             cpx.pixels.fill((0, 0, 0))
             # Generate password (example: a simple number sequence)
-            passw = str(random.randint(1000, 9999))  # Or any other generation method
-            cpx.pixels[0] = (1, 0, 0) # visualize something
+            passw = str(random.randint(1000, 9999))  # Or any other method
+            cpx.pixels[0] = (1, 0, 0)  # visualize something
             while cpx.button_a:  # Debouncing
                 pass
 
         if cpx.button_b:
-            layout.write('{}\n'.format(passw))
+            layout.write("{}\n".format(passw))
             cpx.pixels.fill((0, 1, 0))
             print(passw)
             while cpx.button_b:  # Debouncing
                 pass
 
         if not cpx.switch:
-            break # exit the loop if the switch is turned
+            break  # exit the loop if the switch is turned
+
 
 # def passwords():
 #     # cpx.pixels.fill((0, 1, 1))
@@ -119,14 +117,14 @@ def passwords():
 #     kbd = Keyboard(usb_hid.devices)
 #     layout = KeyboardLayoutUS(kbd)
 
-#     passws = ['1234', '1234', '1234', '1234', '1', '1ll', '1', 'q', 'u$er h4cked y0u']
+#     passws = ['1234',
+#               '1234',
+#               '1234', '1234', '1', '1ll', '1', 'q', 'u$er h4cked y0u']
 
 
 #     passw = ''
 #     p_counter = 0
 #     # after = counter + 1
-    
-    
 #     if cpx.button_a:
 #         cpx.pixels.fill((0, 0, 0))
 #         p_counter += 1
@@ -138,65 +136,73 @@ def passwords():
 #             p_counter = 0
 #         while cpx.button_a:
 #             pass
-    
+
 #     if cpx.button_b:
 #         layout.write('{}\n'.format(passw))
 #         cpx.pixels.fill((0, 1, 0))
 #         print(passw)
 #         while cpx.button_b:
-#             pass   
+#             pass
+
 
 def mode_3():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 3 entered successfully')
+    print("Mode 3 entered successfully")
+
 
 def mode_4():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 4 entered successfully')
+    print("Mode 4 entered successfully")
+
 
 def mode_5():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 5 entered successfully')
+    print("Mode 5 entered successfully")
+
 
 def mode_6():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 6 entered successfully')
+    print("Mode 6 entered successfully")
+
 
 def mode_7():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 7 entered successfully')
+    print("Mode 7 entered successfully")
+
 
 def mode_8():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 8 entered successfully')
+    print("Mode 8 entered successfully")
+
 
 def mode_9():
     cpx.pixels.fill((0, 1, 1))
-    print('Mode 9 entered successfully')
+    print("Mode 9 entered successfully")
+
 
 def gradient_colors():
     for color in range(1, 255):
         if cpx.switch:
             break
-        fill = colwheel(color, brightness=0.1) # colwheel needs to be coded or imported
+        fill = colwheel(color, brightness=0.1)  # colwheel needs to be defined
         cpx.pixels.fill(fill)
         sleep(0.1)
     # if cpx.switch:
-        # break
+    # break
 
 
-modes = [gradient_colors,
-         tone,
-         passwords,
-         joystick,
-         mode_4,
-         mode_5,
-         mode_6,
-         mode_7,
-         mode_8,
-         mode_9]
-
-
+modes = [
+    gradient_colors,
+    tone,
+    passwords,
+    joystick,
+    mode_4,
+    mode_5,
+    mode_6,
+    mode_7,
+    mode_8,
+    mode_9,
+]
 
 counter = 0
 
@@ -205,22 +211,20 @@ while True:
     # print(cpx.switch)
 
     if not cpx.switch:
-        print('switch is turned to right')
+        print("switch is turned to right")
         while not cpx.switch:
             if cpx.switch:
-
                 cpx.pixels.fill((0, 0, 0))
                 cpx.pixels[counter] = (1, 0, 0)
 
                 break
-                
+
             modes[counter]()
-           
+
     else:
         cpx.pixels.fill((0, 0, 0))
         cpx.pixels[counter] = (1, 0, 0)
         if cpx.button_b:
-
             print("b press")
 
             if counter > 9:
@@ -234,7 +238,7 @@ while True:
             cpx.pixels[counter] = (1, 0, 0)
             while cpx.button_b:
                 pass
-            
+
         if cpx.button_a:
             print("a press")
 
@@ -253,7 +257,7 @@ while True:
 
 # Replace the code above with either of these for a single functionality!
 #     #PASSWORD MANAGER#
-#     if cpx.switch:    
+#     if cpx.switch:
 #         if cpx.button_a:
 #             print("a press")
 #             cpx.pixels.fill((0, 0, 0))
@@ -280,8 +284,8 @@ while True:
 #             if i <= -1:
 #                 cpx.pixels[-i+1]=((0,0,1))
 #         sleep(1)
-#         cpx.pixels.fill((0,0,0))    
+#         cpx.pixels.fill((0,0,0))
 #         for i in range(round(cpx.temperature)-round(cpx.temperature/10)*10):
 #             cpx.pixels[i]=((0,1,0))
-#         sleep(1)    
+#         sleep(1)
 #         cpx.pixels.fill((0,0,0))
